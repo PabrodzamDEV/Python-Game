17e1a8437b1845be277f4725a1df65a56f59d2b0
PabrodzamDEV <148392095+PabrodzamDEV@users.noreply.github.com>
2024-01-05 20:11:52 +0100

    Delete GameObjects directory

--- GameObjects/Wall.py
+++
@@ -1,8 +0,0 @@
-import pyglet
-
-
-# Class for the game's walls
-class Wall(pyglet.shapes.Rectangle):
-
-    def __init__(self, *args, **kwargs):
-        super(Wall, self).__init__(*args, **kwargs)
